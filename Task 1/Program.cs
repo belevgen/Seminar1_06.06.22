f098a3b5d837bcdaecf4bf1c366dd9b17e08f85e
belevgen <belsky_evgeniy@mail.rum>
2022-06-14 23:19:56 +0600

    ДЗ Семинар 1_06.06.22_задача 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+string first_str = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string second_str = Console.ReadLine();
+
+double first = double.Parse(first_str);
+double second = double.Parse(second_str);
+
+if (first>second)
+    {
+        Console.WriteLine($"Первое число больше второго" );
+    }
+    else if (first<second)
+    {
+        Console.WriteLine($"Второе число больше первого" );
+    }
+    else 
+    { 
+        Console.WriteLine("Числа рывны");
+    }
