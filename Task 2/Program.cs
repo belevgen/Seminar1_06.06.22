feb1c6549197b2a89de5ba0d4e6f45498ef2be9d
belevgen <belsky_evgeniy@mail.rum>
2022-06-16 00:15:45 +0600

    "ДЗ Семинар 1_06.06.22_задача 2"

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,3 +1,21 @@
 ﻿// Задача 2: Напишите программу, которая принимает на
 // вход три числа и выдаёт максимальное из этих чисел.
-Console.WriteLine("Hello, World!");
+
+Console.Write("Введите первое число: ");
+string first_str = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string second_str = Console.ReadLine();
+Console.Write("Введите третье число: ");
+string third_str = Console.ReadLine();
+
+
+double first = double.Parse(first_str);
+double second = double.Parse(second_str);
+double third = double.Parse(third_str);
+
+double max = first;
+
+if (second > max) { max = second; }
+if (third > max) { max = third; }
+
+Console.WriteLine($"Мамаксимальное число: {max}");
